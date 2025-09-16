1920ee72297c51ee531594d7661364294f48cf84
PRRTZ <147954640+PRRTZ@users.noreply.github.com>
2025-09-16 20:28:42 -0300

    Update simulacao.h
    atualização de funções

--- simulacao.h
+++ simulacao.h
@@ -1,3 +1,11 @@
+#ifndef SIMULACAO_H
+#define SIMULACAO_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
 enum Modo { GLOBAL = 0, LOCAL = 1 };
 enum Algoritmo { FIFO = 0, OPTIMAL = 1, CLOCK = 2 };
 
@@ -12,5 +20,12 @@ struct Frame {
     int page;
     bool valid; // true = ocupado
     bool dirty; // true = pagina alterada
-    bool refbit; // CLOCK - usado pra saber se a pagina foi referenciada   
+    bool refbit; // CLOCK - usado pra saber se a pagina foi referenciada
+    Frame() : pid(-1), page(-1), valid(false), dirty(false), refbit(false) {}
 };
+
+// Funções 
+bool parse_token(const string &tok, Acesso &a);
+void run_simulation(const vector<Acesso> &seq, int total_frames, Modo modO, Algoritmo alg, int num_procs);
+
+#endif
