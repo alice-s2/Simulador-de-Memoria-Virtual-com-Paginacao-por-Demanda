78a68ba79b02e61e0da9d56778ac39c95d0a860d
PRRTZ <147954640+PRRTZ@users.noreply.github.com>
2025-09-17 13:05:23 -0300

    Update util.h
    Atualização das funções

--- util.h
+++ util.h
@@ -1,9 +1,16 @@
+#ifndef UTIL_H
+#define UTIL_H
+
 #include <string>
+#include <vector>
+#include "simulacao.h"
 using namespace std;
 
-extern const string RESET, BLUE, MAG, CYAN; 
+extern const string RESET, BLUE, MAG, CYAN;
 
-string colorir(); // função para colorir strings
-void print_frames(); // função para mostrar os frames
-void cls(); // função para limpar a tela
-void print_titulo(); // função para printar o titulo do simulador
+void cls();
+void print_titulo();
+string colorir(const string &s, const string &code);
+void print_frames(const vector<Frame> &frames, int start, int len, int replaced_idx = -1, int hit_idx = -1, int new_idx = -1); // função para mostrar os frames
+
+#endif
